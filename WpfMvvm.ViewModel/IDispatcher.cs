43f6d96646cd94c088b3cc3205a217f32244482c
EldHasp <EldHasp.WpfMvvm@Gmail.com>
2020-12-03 18:18:35 +0300

    Отредактированны файлы описания. Добавлен интерфейс IDispatcher.

---
+++ WpfMvvm.ViewModel/IDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Threading;
+
+namespace WpfMvvm.ViewModel
+{
+    /// <summary>Интерфейс предоставляющий свойство с диспетчером.</summary>
+    public interface IDispatcher
+    {
+        /// <summary>Диспетчер экземпляра.<br/>
+        /// Если <see langword="null"/> значит для экземпляра 
+        /// не важно в каком потоке он работает.</summary>
+      Dispatcher Dispatcher { get; }
+    }
+}
